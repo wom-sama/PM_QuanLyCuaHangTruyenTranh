fc8bdb29601cd539a6c880af5356c97a8a07ba04
DatPNguynXzyU <asdxz12345678aaa@gmail.com>
2025-10-28 21:41:57 +0700

    Add files via upload
    client1

--- PM_QuanLyCuaHangTruyenTranh/Main/Client.Designer.cs
+++ PM_QuanLyCuaHangTruyenTranh/Main/Client.Designer.cs
@@ -34,10 +34,18 @@
             this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Button3 = new Guna.UI2.WinForms.Guna2Button();
             this.guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
-            this.guna2ShadowPanel1 = new Guna.UI2.WinForms.Guna2ShadowPanel();
-            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.panelMain = new System.Windows.Forms.Panel();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtHoTen = new System.Windows.Forms.TextBox();
+            this.btnEditProfile = new System.Windows.Forms.Button();
+            this.btnChangePFP = new System.Windows.Forms.Button();
+            this.btnSeeOrder = new System.Windows.Forms.Button();
+            this.btnHistory = new System.Windows.Forms.Button();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblHoTen = new System.Windows.Forms.Label();
+            this.picAvatar = new Guna.UI2.WinForms.Guna2CirclePictureBox();
             this.guna2Panel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picAvatar)).BeginInit();
             this.SuspendLayout();
             // 
             // guna2Button1
@@ -84,39 +92,120 @@
             // 
             // guna2Panel1
             // 
-            this.guna2Panel1.Controls.Add(this.guna2PictureBox1);
-            this.guna2Panel1.Controls.Add(this.guna2ShadowPanel1);
+            this.guna2Panel1.Controls.Add(this.picAvatar);
+            this.guna2Panel1.Controls.Add(this.panelMain);
+            this.guna2Panel1.Controls.Add(this.txtEmail);
+            this.guna2Panel1.Controls.Add(this.txtHoTen);
+            this.guna2Panel1.Controls.Add(this.btnEditProfile);
+            this.guna2Panel1.Controls.Add(this.btnChangePFP);
+            this.guna2Panel1.Controls.Add(this.btnSeeOrder);
+            this.guna2Panel1.Controls.Add(this.btnHistory);
+            this.guna2Panel1.Controls.Add(this.lblEmail);
+            this.guna2Panel1.Controls.Add(this.lblHoTen);
             this.guna2Panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.guna2Panel1.Location = new System.Drawing.Point(0, 0);
             this.guna2Panel1.Name = "guna2Panel1";
-            this.guna2Panel1.Size = new System.Drawing.Size(1276, 459);
+            this.guna2Panel1.Size = new System.Drawing.Size(947, 457);
             this.guna2Panel1.TabIndex = 3;
-            this.guna2Panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.guna2Panel1_Paint);
             // 
-            // guna2ShadowPanel1
+            // panelMain
             // 
-            this.guna2ShadowPanel1.BackColor = System.Drawing.Color.Transparent;
-            this.guna2ShadowPanel1.FillColor = System.Drawing.Color.White;
-            this.guna2ShadowPanel1.Location = new System.Drawing.Point(545, 41);
-            this.guna2ShadowPanel1.Name = "guna2ShadowPanel1";
-            this.guna2ShadowPanel1.ShadowColor = System.Drawing.Color.Black;
-            this.guna2ShadowPanel1.Size = new System.Drawing.Size(589, 286);
-            this.guna2ShadowPanel1.TabIndex = 0;
+            this.panelMain.Location = new System.Drawing.Point(427, 44);
+            this.panelMain.Name = "panelMain";
+            this.panelMain.Size = new System.Drawing.Size(512, 401);
+            this.panelMain.TabIndex = 14;
             // 
-            // guna2PictureBox1
+            // txtEmail
             // 
-            this.guna2PictureBox1.ImageRotate = 0F;
-            this.guna2PictureBox1.Location = new System.Drawing.Point(123, 41);
-            this.guna2PictureBox1.Name = "guna2PictureBox1";
-            this.guna2PictureBox1.Size = new System.Drawing.Size(300, 200);
-            this.guna2PictureBox1.TabIndex = 1;
-            this.guna2PictureBox1.TabStop = false;
+            this.txtEmail.Location = new System.Drawing.Point(133, 313);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.ReadOnly = true;
+            this.txtEmail.Size = new System.Drawing.Size(208, 22);
+            this.txtEmail.TabIndex = 13;
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.Location = new System.Drawing.Point(133, 278);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.ReadOnly = true;
+            this.txtHoTen.Size = new System.Drawing.Size(208, 22);
+            this.txtHoTen.TabIndex = 12;
+            // 
+            // btnEditProfile
+            // 
+            this.btnEditProfile.Location = new System.Drawing.Point(22, 249);
+            this.btnEditProfile.Name = "btnEditProfile";
+            this.btnEditProfile.Size = new System.Drawing.Size(147, 23);
+            this.btnEditProfile.TabIndex = 11;
+            this.btnEditProfile.Text = "Chỉnh sửa hồ sơ";
+            this.btnEditProfile.UseVisualStyleBackColor = true;
+            this.btnEditProfile.Click += new System.EventHandler(this.btnEditProfile_Click);
+            // 
+            // btnChangePFP
+            // 
+            this.btnChangePFP.Location = new System.Drawing.Point(22, 206);
+            this.btnChangePFP.Name = "btnChangePFP";
+            this.btnChangePFP.Size = new System.Drawing.Size(165, 23);
+            this.btnChangePFP.TabIndex = 10;
+            this.btnChangePFP.Text = "Thay đổi ảnh đại diện";
+            this.btnChangePFP.UseVisualStyleBackColor = true;
+            this.btnChangePFP.Click += new System.EventHandler(this.btnChangePFP_Click);
+            // 
+            // btnSeeOrder
+            // 
+            this.btnSeeOrder.Location = new System.Drawing.Point(22, 369);
+            this.btnSeeOrder.Name = "btnSeeOrder";
+            this.btnSeeOrder.Size = new System.Drawing.Size(125, 28);
+            this.btnSeeOrder.TabIndex = 9;
+            this.btnSeeOrder.Text = "Xem đơn hàng";
+            this.btnSeeOrder.UseVisualStyleBackColor = true;
+            this.btnSeeOrder.Click += new System.EventHandler(this.btnSeeOrder_Click);
+            // 
+            // btnHistory
+            // 
+            this.btnHistory.Location = new System.Drawing.Point(176, 369);
+            this.btnHistory.Name = "btnHistory";
+            this.btnHistory.Size = new System.Drawing.Size(160, 28);
+            this.btnHistory.TabIndex = 8;
+            this.btnHistory.Text = "Lịch sử mua hàng";
+            this.btnHistory.UseVisualStyleBackColor = true;
+            this.btnHistory.Click += new System.EventHandler(this.btnHistory_Click);
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(29, 319);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(44, 16);
+            this.lblEmail.TabIndex = 5;
+            this.lblEmail.Text = "Email:";
+            // 
+            // lblHoTen
+            // 
+            this.lblHoTen.AutoSize = true;
+            this.lblHoTen.Location = new System.Drawing.Point(29, 278);
+            this.lblHoTen.Name = "lblHoTen";
+            this.lblHoTen.Size = new System.Drawing.Size(73, 16);
+            this.lblHoTen.TabIndex = 4;
+            this.lblHoTen.Text = "Họ và Tên:";
+            // 
+            // picAvatar
+            // 
+            this.picAvatar.ImageRotate = 0F;
+            this.picAvatar.Location = new System.Drawing.Point(22, 12);
+            this.picAvatar.Name = "picAvatar";
+            this.picAvatar.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.picAvatar.Size = new System.Drawing.Size(182, 172);
+            this.picAvatar.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.picAvatar.TabIndex = 15;
+            this.picAvatar.TabStop = false;
             // 
             // Client
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1276, 459);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(947, 457);
             this.Controls.Add(this.guna2Panel1);
             this.Controls.Add(this.guna2Button3);
             this.Controls.Add(this.guna2Button2);
@@ -124,9 +213,9 @@
             this.Name = "Client";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Khachcs";
-            this.Load += new System.EventHandler(this.Khachcs_Load);
             this.guna2Panel1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.guna2Panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picAvatar)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -138,7 +227,15 @@
         private Guna.UI2.WinForms.Guna2Button guna2Button2;
         private Guna.UI2.WinForms.Guna2Button guna2Button3;
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
-        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
-        private Guna.UI2.WinForms.Guna2ShadowPanel guna2ShadowPanel1;
+        private System.Windows.Forms.Button btnSeeOrder;
+        private System.Windows.Forms.Button btnHistory;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Button btnChangePFP;
+        private System.Windows.Forms.Button btnEditProfile;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtHoTen;
+        private System.Windows.Forms.Panel panelMain;
+        private System.Windows.Forms.Label lblHoTen;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox picAvatar;
     }
 }
